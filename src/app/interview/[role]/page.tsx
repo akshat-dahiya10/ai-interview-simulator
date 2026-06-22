824105b94065515824353d2323ca8cd407806aae
akshat-dahiya10 <dahiyaakshat10@gmail.com>
2026-06-22 23:30:13 +0530

    Update page.tsx

--- src/app/interview/[role]/page.tsx
+++ src/app/interview/[role]/page.tsx
@@ -1,24 +1,3 @@
-import { notFound } from "next/navigation";
-import { getRole } from "@/lib/data";
-import ClientPage from "./ClientPage";
-
-export default function Page({
-  params,
-}: {
-  params: { role: string };
-}) {
-  const role = params.role?.toLowerCase().trim();
-
-  console.log("ROLE:", role);
-
-  if (!role) return notFound();
-
-  const roleData = getRole(role);
-
-  if (!roleData) {
-    console.log("NOT FOUND ROLE:", role);
-    return notFound();
-  }
-
-  return <ClientPage role={roleData} />;
+export default function Page() {
+  return <h1>ROLE PAGE WORKING</h1>;
 }
